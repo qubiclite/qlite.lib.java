c7f03e0f5808d94d2ac4d906fe0676ad54d2f5f5
mikrohash.de <iota@mikrohash.de>
2018-08-08 10:21:20 +0200

    update version: v0.3 -> v0.4-SNAPSHOT

--- qlite/src/main/java/constants/GeneralConstants.java
+++ qlite/src/main/java/constants/GeneralConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class GeneralConstants {
 
-    public static final String VERSION = "ql-0.3";
+    public static final String VERSION = "ql-0.4-SNAPSHOT";
 
     public static final int QLVM_MAX_VALUE_LENGTH = 5000;
 
