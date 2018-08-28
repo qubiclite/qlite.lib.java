2b102763b635ac0b43842767af4954c22796775c
mikrohash.de <iota@mikrohash.de>
2018-08-28 13:11:30 +0200

    refactor IAMWriterTest

--- qlite/src/test/java/iam/IAMWriterTest.java
+++ qlite/src/test/java/iam/IAMWriterTest.java
@@ -13,39 +13,65 @@ import java.security.InvalidParameterException;
 
 public class IAMWriterTest {
 
+    private final Method predictAmountOfFragmentsMethod;
+    private final int MAX_CHARS_PER_FRAGMENT;
+
     private final IAMWriter iamWriter = new IAMWriter();
 
-    private int[][] generateTestTableForPredictAmountOfFragments() throws NoSuchFieldException, IllegalAccessException {
-        int MAX_CHARS_PER_FRAGMENT;
+    // === CONSTRUCTION ===
 
-        Field maxCharsPerFragmentField = IAMWriter.class.getDeclaredField("MAX_CHARS_PER_FRAGMENT");
-        maxCharsPerFragmentField.setAccessible(true);
-        MAX_CHARS_PER_FRAGMENT = maxCharsPerFragmentField.getInt(null);
-
-        return new int[][]{
-                {0, 0},
-                {MAX_CHARS_PER_FRAGMENT, 1},
-                {MAX_CHARS_PER_FRAGMENT+1, 2},
-                {MAX_CHARS_PER_FRAGMENT*2-TryteTool.TRYTES_PER_HASH, 2},
-                {MAX_CHARS_PER_FRAGMENT*2-TryteTool.TRYTES_PER_HASH+1, 3},
-                {MAX_CHARS_PER_FRAGMENT*3-TryteTool.TRYTES_PER_HASH*2, 3},
-                {MAX_CHARS_PER_FRAGMENT*3-TryteTool.TRYTES_PER_HASH*2+1, 4},
-        };
+    public IAMWriterTest() throws NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        this.predictAmountOfFragmentsMethod = getPredictAmountOfFragmentsMethod();
+        this.MAX_CHARS_PER_FRAGMENT = getMaxCharsPerFragment();
     }
 
-    @Test
-    public void testPredictAmountOfFragments() throws InvocationTargetException, NoSuchMethodException, NoSuchFieldException, IllegalAccessException {
-
-        int[][] testTable = generateTestTableForPredictAmountOfFragments();
-
+    private static Method getPredictAmountOfFragmentsMethod() throws NoSuchMethodException {
         Method predictAmountOfFragments = IAMWriter.class.getDeclaredMethod("predictAmountOfFragments", Integer.TYPE);
         predictAmountOfFragments.setAccessible(true);
+        return predictAmountOfFragments;
+    }
 
-        for(int[] testRow : testTable) {
-            int prediction = (int)predictAmountOfFragments.invoke(null, testRow[0]);
-            assertEquals("content length: " + testRow[0], testRow[1], prediction);
+    private static int getMaxCharsPerFragment() throws NoSuchFieldException, IllegalAccessException {
+        Field maxCharsPerFragmentField = IAMWriter.class.getDeclaredField("MAX_CHARS_PER_FRAGMENT");
+        maxCharsPerFragmentField.setAccessible(true);
+        return maxCharsPerFragmentField.getInt(null);
+    }
+
+    // === testPredictAmountOfFragments() ===
+
+    @Test
+    public void testPredictAmountOfFragments() {
+        for(int i = 0; i < 10; i++)
+            testCriticalThresholdForPredictAmountOfFragments(i);
+    }
+
+    private void testCriticalThresholdForPredictAmountOfFragments(int subThresholdExpectedAmountOfFragments) {
+        int subThresholdContentLength = determineMaxContentLengthForFragments(subThresholdExpectedAmountOfFragments);
+        testSinglePredictAmountOfFragmentValuePair(subThresholdContentLength, subThresholdExpectedAmountOfFragments);
+        testSinglePredictAmountOfFragmentValuePair(subThresholdContentLength+1, subThresholdExpectedAmountOfFragments+1);
+    }
+
+    private int determineMaxContentLengthForFragments(int fragments) {
+        final int hashBlockLength = Math.max(0, TryteTool.TRYTES_PER_HASH*(fragments-1));
+        final int messageLength = MAX_CHARS_PER_FRAGMENT*fragments;
+        return messageLength-hashBlockLength;
+    }
+
+    private void testSinglePredictAmountOfFragmentValuePair(int contentLength, int expectedAmountOfFragments) {
+        String message = "content length: " + contentLength;
+        int actual = predictAmountOfFragments(contentLength);
+        assertEquals(message, expectedAmountOfFragments, actual);
+    }
+
+    private int predictAmountOfFragments(int contentLength) {
+        try {
+            return (int)predictAmountOfFragmentsMethod.invoke(null, contentLength);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    // === test testNonAsciiMessage ===
 
     @Test(expected = InvalidParameterException.class)
     public void testNonAsciiMessage() {
