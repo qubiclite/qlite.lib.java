90e9f39f7494c2a86b176d591b7545389b305205
mikrohash.de <iota@mikrohash.de>
2018-09-19 14:23:29 +0200

    add optional keyword parameter to ql function iam_read()

--- qlite/src/main/java/qlvm/functions/iam/FunctionIAMRead.java
+++ qlite/src/main/java/qlvm/functions/iam/FunctionIAMRead.java
@@ -13,12 +13,10 @@ public class FunctionIAMRead extends Function {
 
     @Override
     public String call(QLVM qlvm, String[] par) {
-        String iamID = par[0];
-        iamID = iamID.substring(1, iamID.length()-1);
-
+        String iamID = par[0].substring(1, par[0].length()-1);
         int position = parseStringToNumber(par[1]).intValue();
-
-        JSONObject o = new IAMReader(iamID).read(new IAMIndex(position));
+        String keyword = par.length == 2 ? "" : par[2].substring(1, par[2].length()-1);
+        JSONObject o = new IAMReader(iamID).read(new IAMIndex(keyword, position));
         return o == null ? null : o.toString();
     }
 }
