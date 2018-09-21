f388477bec0e673360ab16ed8848048f05d3b56e
mikrohash.de <iota@mikrohash.de>
2018-09-21 17:06:36 +0200

    change version to 0.5.0

--- qlite/src/main/java/constants/GeneralConstants.java
+++ qlite/src/main/java/constants/GeneralConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class GeneralConstants {
 
-    public static final String VERSION = "ql-0.4.1";
+    public static final String VERSION = "ql-0.5.0";
 
     public static final int QLVM_MAX_VALUE_LENGTH = 5000;
 
